8be00aabe22639f60806d3badc72b6a77eb37941
sisisin <azsisisin@gmail.com>
2020-04-26 18:27:29 +0900

    feat: simple fussy search

--- src/views/ChannelViewer.vue
+++ src/views/ChannelViewer.vue
@@ -4,14 +4,19 @@
     <div>token: <input name="token" type="text" v-model="state.token" /></div>
     <div><input type="button" value="start" @click="start" /></div>
     <hr />
-    exclude joined channel
-    <input
-      name="exclude_joined_channel"
-      type="checkbox"
-      v-model="state.excludeJoinedChannel"
-    />
+    <div>
+      exclude joined channel
+      <input
+        name="exclude_joined_channel"
+        type="checkbox"
+        v-model="state.excludeJoinedChannel"
+      />
+    </div>
+    <div>
+      serch: <input name="search" type="text" v-model="state.searchText" />
+    </div>
     <ul>
-      <li v-for="channel in filteredChannels" v-bind:key="channel.id">
+      <li v-for="channel in searched" v-bind:key="channel.id">
         {{ channel.name }}
       </li>
     </ul>
@@ -21,27 +26,39 @@
 <script lang="ts">
 import { reactive, defineComponent, computed } from '@vue/composition-api';
 import { slackClient, Conversation } from '@/services/slackClient';
+import Fuse from 'fuse.js';
 
 const key = 'slack_token';
+
 export default defineComponent({
   setup() {
     const state = reactive({
       token: localStorage.getItem(key) ?? '',
-      excludeJoinedChannel: true,
-      channels: [] as Conversation[]
+      excludeJoinedChannel: false,
+      searchText: '',
+      channels: [] as Conversation[],
+      searcher: null as Fuse<
+        Conversation,
+        Fuse.IFuseOptions<Conversation>
+      > | null
     });
-    const filteredChannels = computed(() =>
-      state.channels.filter(c => {
-        if (state.excludeJoinedChannel) {
-          return c.is_member;
-        } else {
-          return true;
-        }
-      })
-    );
+    const filteredChannels = computed(() => {
+      if (!state.excludeJoinedChannel) {
+        return state.channels;
+      }
+      return state.channels.filter(c => {
+        return c.is_member;
+      });
+    });
+    const searched = computed(() => {
+      if (state.searcher === null || state.searchText === '') {
+        return filteredChannels.value;
+      }
+      return state.searcher.search(state.searchText).map(s => s.item);
+    });
     return {
       state,
-      filteredChannels,
+      searched,
       async start() {
         localStorage.setItem(key, state.token);
         const res = await slackClient.listConversations({
@@ -49,6 +66,7 @@ export default defineComponent({
           limit: 1000
         });
         state.channels = res.channels;
+        state.searcher = new Fuse(filteredChannels.value, { keys: ['name'] });
       }
     };
   }
